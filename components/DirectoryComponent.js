9dc3db80b1920fb8d060fc436b17e43e5ff800ae
Nhan Phan <nphanacc@gmail.com>
2020-11-28 09:50:15 -0800

    PreReactNativeWorkshop2

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,25 +1,58 @@
-import React from "react";
-import { FlatList } from "react-native";
-import { ListItem } from "react-native-elements";
+import React, { Component } from "react";
+import { FlatList, View, Text } from "react-native";
+import { ListItem, Tile } from "react-native-elements";
+import { connect } from "react-redux";
+import { baseUrl } from "../shared/baseUrl";
+import Loading from "./LoadingComponent";
 
-function Directory(props) {
-	const renderDirectoryItem = ({ item }) => {
-		return (
-			<ListItem
-				title={item.name}
-				subtitle={item.description}
-				leftAvatar={{ source: require("./images/react-lake.jpg") }}
-			/>
-		);
+const mapStateToProps = (state) => {
+	return {
+		campsites: state.campsites,
+	};
+};
+
+class Directory extends Component {
+	static navigationOptions = {
+		title: "Directory",
 	};
 
-	return (
-		<FlatList
-			data={props.campsites}
-			renderItem={renderDirectoryItem}
-			keyExtractor={(item) => item.id.toString()}
-		/>
-	);
+	render() {
+		const { navigate } = this.props.navigation;
+		const renderDirectoryItem = ({ item }) => {
+			return (
+				<Tile
+					title={item.name}
+					// subtitle={item.description} subtitle only work for ListItem. usage of caption and feature for Title
+					caption={item.description}
+					featured
+					onPress={() => navigate("CampsiteInfo", { campsiteId: item.id })}
+					imageSrc={{ uri: baseUrl + item.image }}
+					// onPress={() => props.onPress(item.id)} //ListItem has its own onPress property and we also use onPress props aka onCampsiteSelect method from main
+					// leftAvatar={{ source: require("./images/react-lake.jpg") }} work for ListItem only
+				/>
+			);
+		};
+
+		if (this.props.campsites.isLoading) {
+			return <Loading />;
+		}
+
+		if (this.props.campsites.errMess) {
+			return (
+				<View>
+					<Text>{this.props.campsites.errMess}</Text>
+				</View>
+			);
+		}
+
+		return (
+			<FlatList
+				data={this.props.campsites.campsites}
+				renderItem={renderDirectoryItem}
+				keyExtractor={(item) => item.id.toString()}
+			/>
+		);
+	}
 }
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ components/LoadingComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+
+function Loading() {
+	return (
+		<View style={style.loadingView}>
+			<ActivityIndicator size="large" color="#5637DD" />
+			<Text style={style.loadingText}>Loading . . .</Text>
+		</View>
+	);
+}
+
+const style = StyleSheet.create({
+	loadingView: {
+		alignItems: "center",
+		justifyContent: "center",
+		flex: 1,
+	},
+	loadingText: {
+		color: "#5637DD",
+		fontSize: 14,
+		fontWeight: "bold",
+	},
+});
+
+export default Loading;
